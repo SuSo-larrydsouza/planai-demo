497edd0b2331491e7596b2fa63d1bb4f5470ffe7
fmarsault <felix.marsault@plana.earth>
2023-11-29 01:17:21 +0100

    feat: add scrappy password

--- planA_streamlit.py
+++ planA_streamlit.py
@@ -1,3 +1,4 @@
+import hmac
 import os
 from typing import cast
 
@@ -11,6 +12,38 @@ from canopy.models.api_models import (
 from canopy.models.data_models import UserMessage, AssistantMessage
 from canopy.tokenizer import Tokenizer
 
+
+###################################################################################################
+# Scrappy password protection
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+###################################################################################################
+# Main Streamlit app starts here
 st.title("♻️ PlanAI")
 
 planAI_avatar = "img/planA_logo.jpg"
